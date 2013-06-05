2ea372e84feb206d15a68f52689b2d0e3120b327
Ryan Shaw <ryanshaw@unc.edu>
2013-06-05 15:34:41 -0400

    Initial commit.

---
+++ hstore.py
@@ -0,0 +1,83 @@
+import psycopg2
+import psycopg2.extras
+import psycopg2.extensions
+from collections import MutableMapping
+
+def open(connection_uri, name):
+    c = psycopg2.connect(connection_uri)
+    c.cursor().execute('CREATE EXTENSION IF NOT EXISTS hstore')
+    psycopg2.extras.register_hstore(c)
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODE, c)
+    return Hstore(c, name)
+
+TABLE_NAME = 'hstores'
+
+class Hstore(MutableMapping):
+
+    def __init__(self, connection, name):
+        self.connection = connection
+        self.name = name
+        with self.connection as con, con.cursor() as c:
+            c.execute("""
+CREATE TABLE IF NOT EXISTS {table} (
+  name text   PRIMARY KEY,
+  data hstore NOT NULL DEFAULT hstore('') )
+""".format(table=TABLE_NAME))
+            c.execute("""
+INSERT INTO {table} (name) 
+SELECT (%s) WHERE NOT EXISTS (SELECT name FROM {table} WHERE name = %s)
+""".format(table=TABLE_NAME), (self.name, self.name))
+
+    def __getitem__(self, key):
+        if not isinstance(key, basestring):
+            raise TypeError('keys must be strings')
+        with self.connection.cursor() as c: 
+            c.execute("""
+SELECT data -> %s FROM {table} 
+WHERE name = %s
+""".format(table=TABLE_NAME), (key, self.name))
+            value = c.fetchone()[0]
+            if value is None:
+                raise KeyError(key)
+            return value
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, basestring):
+            raise TypeError('keys must be strings')
+        if not isinstance(value, basestring):
+            raise TypeError('values must be strings')
+        with self.connection as con, con.cursor() as c:
+            c.execute("""
+UPDATE {table} SET data = data || hstore(%s, %s) 
+WHERE name = %s
+""".format(table=TABLE_NAME), (key, value, self.name))
+
+    def __delitem__(self, key):
+        if not isinstance(key, basestring):
+            raise TypeError('keys must be strings')
+        with self.connection as con, con.cursor() as c:
+            c.execute("""
+UPDATE {table} SET data = delete(data, %s) 
+WHERE name = %s
+""".format(table=TABLE_NAME), (key, self.name))
+
+    def __iter__(self):
+        with self.connection.cursor() as c: 
+            c.execute("""
+SELECT skeys(data) FROM {table}
+WHERE name = %s
+""".format(table=TABLE_NAME), (self.name,))
+            for r in c:
+                yield r[0]
+
+    def __len__(self):
+        with self.connection.cursor() as c: 
+            c.execute("""
+SELECT array_length(akeys(data), 1) FROM {table}
+WHERE name = %s
+""".format(table=TABLE_NAME), (self.name,))
+            return c.fetchone()[0] or 0
+
+    def close(self):
+        self.connection.close()
+        
---
+++ tests.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+import os
+import hstore
+import psycopg2
+from psycopg2.extensions import \
+    ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_COMMITTED
+from unittest import TestCase
+
+
+connection_uri = os.environ.get(
+    'DBURI', 'postgresql://unittest@localhost/hstore_test')
+
+class HstoreTestCase(TestCase):
+
+    def execute(self, command):
+        if command in [ 'CREATE', 'DROP' ]:
+            server, dbname = connection_uri.rsplit('/',1) 
+            c = psycopg2.connect('{}/template1'.format(server))
+            try:
+                c.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+                c.cursor().execute('{} DATABASE {}'.format(command, dbname))
+                c.set_isolation_level(ISOLATION_LEVEL_READ_COMMITTED)
+            finally:
+                c.close()
+    
+    def setUp(self):
+        #try:
+        #    self.execute('DROP')
+        #except psycopg2.ProgrammingError:
+        #    pass # does not exist
+        self.execute('CREATE')
+        self.hstores = []
+
+    def open_hstore(self, connection_uri, name):
+        d = hstore.open(connection_uri, name)
+        self.hstores.append(d)
+        return d
+
+    def test_open(self):
+        d = self.open_hstore(connection_uri, 'test')
+        
+    def test_open_and_close(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d.close()
+        
+    def test_open_and_close_twice(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d.close()
+        d.close()
+
+    def test_reopen(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d.close()
+        d = self.open_hstore(connection_uri, 'test')
+
+    def test_store_value(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d['foo'] = 'bar'
+        
+    def test_store_and_retrieve_value(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d['foo'] = 'bar'
+        d.close()
+        d = self.open_hstore(connection_uri, 'test')
+        self.assertEqual(d['foo'], 'bar')
+
+    def test_store_and_retrieve_unicode(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d['foo'] = u'日本語'
+        d.close()
+        d = self.open_hstore(connection_uri, 'test')
+        self.assertEqual(d['foo'], u'日本語')
+
+    def test_unicode_key(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d[u'日本語'] = 'foo'
+        d.close()
+        d = self.open_hstore(connection_uri, 'test')
+        self.assertEqual(d[u'日本語'], 'foo')
+        
+    def test_unicode_storename(self):
+        d = self.open_hstore(connection_uri, u'日本語')
+        d['bar'] = 'foo'
+        d.close()
+        d = self.open_hstore(connection_uri, u'日本語')
+        self.assertEqual(d['bar'], 'foo')
+
+    def test_store_nonstring(self):
+        d = self.open_hstore(connection_uri, 'test')
+        with self.assertRaises(TypeError):
+            d['foo'] = 7
+
+    def test_nonstring_key(self):
+        d = self.open_hstore(connection_uri, 'test')
+        with self.assertRaises(TypeError):
+            d[7] = 'foo'
+
+    def test_get_nonexistent_key(self):
+        d = self.open_hstore(connection_uri, 'test')
+        with self.assertRaises(KeyError):
+            d['notthere']
+
+    def test_delete_key(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d['foo'] = 'bar'
+        self.assertEqual(d['foo'], 'bar')
+        del d['foo']
+        with self.assertRaises(KeyError):
+            d['foo']
+
+    def test_length(self):
+        d = self.open_hstore(connection_uri, 'test')
+        self.assertEqual(len(d), 0)
+        d['foo'] = 'bar'
+        self.assertEqual(len(d), 1)
+        d['foo'] = 'baz'
+        self.assertEqual(len(d), 1)
+        d['bar'] = 'baz'
+        self.assertEqual(len(d), 2)
+
+    def test_iterate(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d['foo'] = 'bar'
+        d['bar'] = 'baz'
+        self.assertEqual(sorted(d.iteritems()), 
+                         [(u'bar',u'baz'),(u'foo',u'bar')])
+
+    def test_contains(self):
+        d = self.open_hstore(connection_uri, 'test')
+        self.assertFalse('foo' in d)
+        d['foo'] = 'bar'
+        self.assertTrue('foo' in d)
+        del d['foo']
+        self.assertFalse('foo' in d)
+
+    def test_clear(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d['foo'] = 'bar'
+        d['bar'] = 'baz'
+        self.assertEqual(len(d), 2)
+        d.clear()
+        self.assertEqual(len(d), 0)
+
+    def test_update(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d.update({'foo':'bar','bar':'baz'})
+        self.assertEqual(len(d), 2)
+        self.assertEqual(d['foo'], 'bar')
+        self.assertEqual(d['bar'], 'baz')
+
+    def test_equality(self):
+        d = self.open_hstore(connection_uri, 'test')
+        d.update({'foo':'bar','bar':'baz'})
+        self.assertEqual(d, {'foo':'bar','bar':'baz'})
+
+    def tearDown(self):
+        [ h.close() for h in self.hstores ]
+        self.hstores = []
+        self.execute('DROP')
+        
